488181782e4e16db69528f6ea434d96fb1b1f685
Alex Landherr <alex.landherr@gmail.com>
2022-03-31 13:38:22 +0200

    Created necessary files for website etc.

---
+++ src/ts/RemoveToDo.ts
@@ -0,0 +1,3 @@
+/* export function removeToDo(): void {
+
+} */
---
+++ src/ts/main.ts
@@ -0,0 +1,22 @@
+
+
+//Hard-coded todo items.
+let ToDoItm_1 = document.createElement("li");
+ToDoItm_1.setAttribute("id", "ToDoItm_1");
+ToDoItm_1.innerHTML = "Perform warp core maintenance.";
+document.getElementById("to-do-list").appendChild(ToDoItm_1);
+
+let ToDoItm_2 = document.createElement("li");
+ToDoItm_2.setAttribute("id", "ToDoItm_2");
+ToDoItm_2.innerHTML = "Purge plasma intake manifold.";
+document.getElementById("to-do-list").appendChild(ToDoItm_2);
+
+let ToDoItm_3 = document.createElement("li");
+ToDoItm_3.setAttribute("id", "ToDoItm_3");
+ToDoItm_3.innerHTML = "Check EPS conduits.";
+document.getElementById("to-do-list").appendChild(ToDoItm_3);
+
+let ToDoItm_4 = document.createElement("li");
+ToDoItm_4.setAttribute("id", "ToDoItm_4");
+ToDoItm_4.innerHTML = "Take the con.";
+document.getElementById("to-do-list").appendChild(ToDoItm_4);
